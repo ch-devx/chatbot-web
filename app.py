29db211f5c6cf6f628c8ad3798f84be4ad664d3d
Carlos Heredia <ch.devx@gmail.com>
2025-12-07 18:17:39 -0400

    Add initial Flask application with basic routing and HTML template

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+from google import genai
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    user_input = request.args.get("user_input")
+    response = request.args.get("response")
+    return render_template("index.html", user_input=user_input, response=response)
+
+
+@app.route("/response", methods=["POST"])
+def show_response():
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        response = make_call(user_input)
+        return redirect(url_for("index", user_input=user_input, response=response))
+
+
+def make_call(user_input):
+    client = genai.Client()
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=user_input
+    )
+    return response.text
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
