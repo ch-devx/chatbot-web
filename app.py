799f95093e6f08e84e60a85ca0edc25a47ae4549
Carlos Heredia <carlosheredia2709@gmail.com>
2026-06-23 21:53:18 -0400

    fix(app.py): import session from flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from google import genai
 
 app = Flask(__name__)
